60725494ff873087afae5872a2f73b86e6051e4b
hongsp88 <hongsp88@nate.com>
2023-04-18 19:53:09 +0900

    Sample -

--- 1st_project/src/index.js
+++ 1st_project/src/index.js
@@ -7,7 +7,9 @@ import ReactDOM from 'react-dom/client';
 //import App from './0406_2/App';
 //import App from './0406_3/App';
 //import App from './0407/App';
-import App from './0407_2/App';
+//import App from './0407_2/App';
+import App from './myTodoList/App';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
