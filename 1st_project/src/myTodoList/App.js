7ec1f032c250e3feb9e0ad4cf1f3bfc662f80fd1
hsp220097 <hsp220097@yura.co.kr>
2023-04-20 16:19:21 +0900

    commit test

--- 1st_project/src/myTodoList/App.js
+++ 1st_project/src/myTodoList/App.js
@@ -3,6 +3,7 @@ import TodoListTemplate from './components/TodoListTemplate';
 import Form from './components/Form'
 import TodoItemList from './components/TodoItemList';
 
+const colors = ['#343a40', '#f03e3e', '#12b886', '#228ae6'];
 class App extends Component {
   
   id = 3 // 이미 0,1,2 가 존재하므로 3으로 설정
