3f0fbf4f5b22d4985663cb1413f7e3d885803031
hsp220097 <hsp220097@yura.co.kr>
2023-05-03 19:03:57 +0900

    Sample - styling

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,10 +1,19 @@
 import { Component } from 'react';
 import './App.css';
 import SassComponent from './SassComponent';
+import CSSModule from './CSSModule';
+
+import StyledComponent from './StyledComponent';
 
 class App extends Component {
   render() {
-    return <SassComponent />
+    return (
+      <div>
+        <CSSModule />
+        <SassComponent />
+        <StyledComponent />
+      </div>
+    )
   }
 }
 
