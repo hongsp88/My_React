182acfb63abd10c5ff82f787e840795716ff9191
hongsp88 <hongsp88@nate.com>
2023-06-06 22:02:14 +0900

    Sample - redux-starter-kit. Promise, Pender

--- redux-starter-kit/src/App.js
+++ redux-starter-kit/src/App.js
@@ -2,18 +2,49 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as counterActions from './modules/counter';
-
+import * as postActions from './modules/post'
 
 class App extends Component {
-    render() {
-        const { CounterActions, number } = this.props;
+    componentDidMount() {
+        // 컴포넌트가 처음 마운트 될 때 현재 number 를 postId 로 사용하여 포스트 내용을 불러옵니다.
+        const { number } = this.props;
+        this.getPost(number);
+    }
 
-        
+    componentWillReceiveProps(nextProps) {
+        if(this.props.number !== nextProps.number) {
+            this.getPost(nextProps.number)
+        }
+    }
+
+    getPost = async (postId) => {
+        const { PostActions } = this.props;
+
+        try {
+            await PostActions.getPost(postId);
+            console.log('요청이 완료 된 다음에 실행됨')
+        } catch(e) {
+            console.log('에러가 발생!');
+        }
+    }
+
+    render() {
+        const { CounterActions, number, post, error, loading } = this.props;
+
         return (
             <div>
-                <h1>{number}</h1>
-                <button onClick={CounterActions.incrementAsync}>+</button>
-                <button onClick={CounterActions.decrementAsync}>-</button>
+                <p>{number}</p>
+                <button onClick={CounterActions.increment}>+</button>
+                <button onClick={CounterActions.decrement}>-</button>
+                { loading && <h2>loading...</h2>}
+                { error 
+                    ? <h1>Error!</h1>
+                    : (
+                        <div>
+                            <h1>{post.title}</h1>
+                            <p>{post.body}</p>
+                        </div>
+                )}
             </div>
         );
     }
@@ -21,9 +52,13 @@ class App extends Component {
 
 export default connect(
     (state) => ({
-        number: state.counter
+        number: state.counter,
+        post: state.post.data,
+        loading: state.pender.pending['GET_POST'],
+        error: state.pender.failure['GET_POST']
     }),
     (dispatch) => ({
-        CounterActions: bindActionCreators(counterActions, dispatch)
+        CounterActions: bindActionCreators(counterActions, dispatch),
+        PostActions: bindActionCreators(postActions, dispatch)
     })
 )(App);
--- redux-starter-kit/src/store.js
+++ redux-starter-kit/src/store.js
@@ -4,6 +4,8 @@ import modules from './modules';
 
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk'
+//import promiseMiddleware from 'redux-promise-middleware'
+import penderMiddleware from 'redux-pender'
 
 /* 로그 미들웨어를 생성 할 때 설정을 커스터마이징 할 수 있습니다.
    https://github.com/evgenyrodionov/redux-logger#options
@@ -12,7 +14,10 @@ import ReduxThunk from 'redux-thunk'
 // 미들웨어가 여러개인경우에는 파라미터로 여러개를 전달해주면 됩니다. 예: applyMiddleware(a,b,c)
 // 미들웨어의 순서는 여기서 전달한 파라미터의 순서대로 지정됩니다.
 const logger = createLogger();
+/*const customizedPromiseMiddleware = promiseMiddleware({
+    promiseTypeSuffixes: ['PENDING', 'SUCCESS', 'FAILURE']
+});*/
 
-const store = createStore(modules, applyMiddleware(logger, ReduxThunk))
+const store = createStore(modules, applyMiddleware(logger, ReduxThunk, penderMiddleware()));
 
 export default store;
